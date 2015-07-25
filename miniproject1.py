794786d9196fb677ade264b00fe9dfeee979084e
estelle3627 <canary_191@hotmail.com>
2015-07-25 01:28:47 -0400

    Update miniproject1.py

--- miniproject1.py
+++ miniproject1.py
@@ -1,5 +1,4 @@
-# Rock-paper-scissors-lizard-Spock template
-import random
+# Rock-paper-scissors-lizard-Spock
 
 # The key idea of this program is to equate the strings
 # "rock", "paper", "scissors", "lizard", "Spock" to numbers
@@ -11,85 +10,84 @@ import random
 # 3 - lizard
 # 4 - scissors
 
+# import the random library at the beginning
+import random
+
 # helper functions
 
 def number_to_name(number):
-    # fill in your code below
-    
-    # convert number to a name using if/elif/else
-    # don't forget to return the result!
+    '''
+    This helper function translates number into name.
+    Number has to be 0, 1, 2, 3 or 4
+    '''
     if number == 0:
-        return 'rock'
+        name = 'rock'
     elif number == 1:
-        return 'Spock'
+        name = 'Spock'
     elif number == 2:
-        return 'paper'
+        name = 'paper'
     elif number == 3:
-        return 'lizard'
+        name = 'lizard'
     elif number == 4:
-        return 'scissors'
+        name = 'scissors'
     else:
-        print  ' Invalid entry'
+        name = ''
+        print 'Please provide correct number'
     
-    
+    return name
+
+
 def name_to_number(name):
-    # fill in your code below
-
-    # convert name to number using if/elif/else
-    # don't forget to return the result!
+    '''
+    This helper function translates name to number.
+    The input name has to be valid for the game.
+    '''
     if name == 'rock':
-        return 0
+        number = 0
     elif name == 'Spock':
-        return 1
+        number = 1        
     elif name == 'paper':
-        return 2
+        number = 2        
     elif name == 'lizard':
-        return 3
+        number = 3        
     elif name == 'scissors':
-        return 4
+        number = 4        
     else:
-        print  ' Invalid entry'
+        number = -1
+        print 'Please provide correct name'
     
-
-
+    return number
+    
+    
 def rpsls(name): 
-    # fill in your code below
-
-    # convert name to player_number using name_to_number
+    '''
+    The main function that runs the game.
+    '''
     player_number = name_to_number(name)
-    
     # compute random guess for comp_number using random.randrange()
-    comp_number = random.randrange(0,5)
-
+    comp_number = random.randrange(0, 5)
     # compute difference of player_number and comp_number modulo five
-    diff = (player_number - comp_number) % 5
-
+    diff_mod_5 = (player_number - comp_number) % 5
     # use if/elif/else to determine winner
-    if diff == 1:
-        winner = 'Player wins!'
-    elif diff == 2:
-        winner = 'Player wins!'      
-    elif diff == 0:
-        winner = 'Player and computer tie!'
+    if diff_mod_5 == 1 or diff_mod_5 == 2:
+        player_wins = 1
+    elif diff_mod_5 == 3 or diff_mod_5 == 4:
+        player_wins = -1
     else:
-        winner = 'Computer wins!'
-        
+        player_wins = 0
     # convert comp_number to name using number_to_name
     comp_name = number_to_name(comp_number)
-    
     # print results
-    print 'Player chooses ' + name
-    print 'Computer chooses ' + comp_name
+    print "Player chooses " + name
+    print "Computer chooses " + comp_name
     if player_wins == 1:
         print "Player wins!"
     elif player_wins == -1:
-		    print "Computer wins!"
-	  else:
-	      print "Player and computer tie!"
-# print a new line, in order to match the sample output.    
-        print ""
-
-    
+        print "Computer wins!"
+    else:
+        print "Player and computer tie!"
+    # print a new line, in order to match the sample output.    
+    print ""
     
 # test your code
 rpsls("rock")
@@ -99,5 +97,3 @@ rpsls("lizard")
 rpsls("scissors")
 
 # always remember to check your completed program against the grading rubric
-
-
